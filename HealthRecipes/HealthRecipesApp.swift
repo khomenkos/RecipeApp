999eec7fefedbecb3cbb564b9c744f8080e61ef5
khomenkos <khomenkos196@gmail.com>
2021-01-19 17:54:36 +0200

    Initial Commit

---
+++ HealthRecipes/HealthRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthRecipesApp.swift
+//  HealthRecipes
+//
+//  Created by  Sasha Khomenko on 19.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct HealthRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
